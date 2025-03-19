670e6bde98e52b4795528f7dd8e47d1e8d44d1f6
Avanish Singh <savanish67@gmail.com>
2025-03-20 00:47:11 +0530

    Add and Commit Data Science to Repo

---
+++ Pandas/EDA.py
@@ -0,0 +1,6 @@
+import pandas as pd
+df = pd.read_csv("E:\playground\python_playgroung_codes\Data_Science\Pandas\Training_Data(CSV-file)\weather.csv")
+print(df)
+
+df1 = pd.read_csv("E:\playground\python_playgroung_codes\Data_Science\Pandas\Training_Data(CSV-file)\infolimpioavanzadoTarget.csv")
+print(df1)
